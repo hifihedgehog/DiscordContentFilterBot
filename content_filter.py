9bb6feebb15005667162a84a3a81b041f24146de
hifihedgehog <16614343+hifihedgehog@users.noreply.github.com>
2024-11-27 11:49:21 -0500

    Tweaking punishment log and DM formatting

--- content_filter.py
+++ content_filter.py
@@ -1557,7 +1557,7 @@ async def check_and_apply_punishment(user: discord.Member, guild_id: int, server
     if len(recent_violations) >= server_config["punishments"]["max_violations"]:
         role_id = server_config["punishments"]["punishment_role"]
         punishment_duration = server_config["punishments"]["punishment_duration"]
-
+        punishment_duration_str = await format_timedelta(punishment_duration)
         if role_id:
             role = user.guild.get_role(role_id)
             if role:
@@ -1580,7 +1580,7 @@ async def check_and_apply_punishment(user: discord.Member, guild_id: int, server
                 embed_punish_1 = discord.Embed(title=f"{user.guild.name} Discord Server Content Filter Notification", description ="You have received a temporary role due to repeated violations of the server's content rules.", color=discord.Color.dark_red())
                 embed_punish_2 = discord.Embed(title="Punishment Applied", color=discord.Color.dark_red(), timestamp=current_time)
                 embed_punish_2.add_field(name="Punishment Role", value=f"`{role.name}`", inline=False)
-                embed_punish_2.add_field(name="Duration", value=str(punishment_duration), inline=False)
+                embed_punish_2.add_field(name="Duration", value=punishment_duration_str, inline=False)
                 embed_punish_2.add_field(name="Punishment Expires At", value=f"<t:{int(expiration_time.timestamp())}:R>", inline=False)
                 embed_punish_2.add_field(name="Reason", value="Repeated violations of the server's content rules.", inline=False)
                 embeds.append(embed_punish_1)
@@ -1598,7 +1598,7 @@ async def check_and_apply_punishment(user: discord.Member, guild_id: int, server
                         embed_log = discord.Embed(title="Punishment Applied", color=discord.Color.dark_red(), timestamp=current_time)
                         embed_log.add_field(name="User", value=user.mention, inline=False)
                         embed_log.add_field(name="Punishment Role", value=f"{role.mention}", inline=False)
-                        embed_log.add_field(name="Duration", value=str(punishment_duration), inline=False)
+                        embed_log.add_field(name="Duration", value=punishment_duration_str, inline=False)
                         embed_log.add_field(name="Punishment Expires At", value=f"<t:{int(expiration_time.timestamp())}:R>", inline=False)
                         embed_log.add_field(name="Reason", value="Repeated violations of the server's content rules.", inline=False)
                         await log_channel.send(embed=embed_log)
